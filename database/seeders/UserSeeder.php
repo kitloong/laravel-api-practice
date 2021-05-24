8641ff46e5340e39b2bd62e58891107fbe79c059
kitloong <kitloong1008@hotmail.com>
2021-05-24 22:37:35 +0800

    Vup to Laravel 8

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,11 +1,9 @@
 <?php
+
 namespace Database\Seeders;
 
-use Carbon\Carbon;
+use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -16,14 +14,8 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 0; $i < 10; $i++) {
-            DB::table('users')->insert([
-                'name' => Str::random(10),
-                'email' => Str::random(10).'@gmail.com',
-                'password' => Hash::make('password'),
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ]);
-        }
+        User::factory()
+            ->count(50)
+            ->create();
     }
 }
